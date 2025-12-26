2302e14a217544387bb805edfed19f807bd79dd9
sazzad <sazzadador@gmail.com>
2025-12-26 09:57:00 +0600

    total seat count

--- js/app.js
+++ js/app.js
@@ -1,15 +1,20 @@
 console.log("hello");
-const selectedSeatEl = document.getElementById("selected-seat");
+const selectedSeatEl = document.getElementById("selected-seat")
+const totalSeat = document.getElementById("total-booked-seat");
+
+let SelectedSeat = [];
 function handleSelectSeat(event) {
   console.log(event.innerText);
   event.classList.add("my-bg-primary");
   event.classList.add("text-white");
-  
+  SelectedSeat.push(event.innerText);
+  console.log(SelectedSeat);
+  totalSeat.innerText = SelectedSeat.length;
 
-  // selectedSeatEl.innerHTML = `<li class="flex justify-between text-base font-normal">
-  //   <span>${event.innerText}</span>
-  //   <span>Economy</span>
-  //   <span>550</span>
+  selectedSeatEl.innerHTML = `<li class="flex justify-between text-base font-normal">
+    <span>${event.innerText}</span>
+    <span>Economy</span>
+    <span>550</span>
 
-  // </li>`;
+  </li>`;
 }
