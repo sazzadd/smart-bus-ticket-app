6dca9075b124e1c827e2d2ff73e6cc315cca5442
sazzad <sazzadador@gmail.com>
2026-01-03 21:05:21 +0600

    conditional booked aplly

--- src/js/app.js
+++ src/js/app.js
@@ -2,30 +2,47 @@ console.log("hello");
 const selectedSeatEl = document.getElementById("selected-seat");
 const totalSeat = document.getElementById("total-booked-seat");
 const availableSeatEl = document.getElementById("available-seat");
+const totalPriceEl = document.getElementById("total-price");
+const couponInputEl = document.getElementById("coupon-input-field");
+const couponBtn = document.getElementById("coupon-btn");
 
 let SelectedSeat = [];
- let totalPrice = 0;
+let totalPrice = 0;
 function handleSelectSeat(event) {
-  console.log(event.innerText);
-  event.classList.add("my-bg-primary");
-  event.classList.add("text-white");
-  SelectedSeat.push(event.innerText);
-  console.log(SelectedSeat);
-  totalSeat.innerText = SelectedSeat.length;
- 
-  selectedSeatEl.innerHTML += `<li class="flex justify-between text-base font-normal">
+  const value = event.innerText;
+  if (SelectedSeat.includes(value)) {
+    return alert("seat alredy booked");
+  } else if (SelectedSeat.length < 4) {
+    console.log(event.innerText);
+    event.classList.add("my-bg-primary");
+    event.classList.add("text-white");
+    SelectedSeat.push(event.innerText);
+    console.log(SelectedSeat);
+    totalSeat.innerText = SelectedSeat.length;
+
+    selectedSeatEl.innerHTML += `<li class="flex justify-between text-base font-normal">
     <span>${event.innerText}</span>
     <span>Economy</span>
     <span>550</span>
 
   </li>`;
 
-  // decress avvailable seat
-  const availableSeatValue = parseFloat(availableSeatEl.innerText);
-  console.log(typeof availableSeatValue);
-  const newAvailableSeat = availableSeatValue - 1;
-  availableSeatEl.innerText = newAvailableSeat;
+    // decress avvailable seat
+    const availableSeatValue = parseFloat(availableSeatEl.innerText);
+    console.log(typeof availableSeatValue);
+    const newAvailableSeat = availableSeatValue - 1;
+    availableSeatEl.innerText = newAvailableSeat;
 
-  // updated price
-   totalPrice += 550;
+    // updated price
+    totalPrice += 550;
+    totalPriceEl.innerText = totalPrice.toFixed(2);
+
+    // active coupon button
+    if (SelectedSeat.length > 3) {
+      couponInputEl.removeAttribute("disabled");
+      couponBtn.removeAttribute("disabled");
+    }
+  } else {
+    alert("maximum seat booked");
+  }
 }
